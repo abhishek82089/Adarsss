32a35c6438959fe8c12bcdafce53d1c843041e67
AkRao47 <159660084+AkRao47@users.noreply.github.com>
2024-04-29 07:31:52 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id = "25298674"
-api_hash = "1a7e01278b2bf0fe975fb8ce7c5ead8d"
-bot_token = "7092396095:AAF_jP7qZx_cC0esduKkPafHGt9vhsrWxiM"
+api_id ="23421759"# "25298674"
+api_hash ="ba8ae5b1f703fc771d1e8425db8752d1" # "1a7e01278b2bf0fe975fb8ce7c5ead8d"
+bot_token ="7129059177:AAFVZ8VQZn6VaeinkkXAgHneWySAbMrD36M"# "7092396095:AAF_jP7qZx_cC0esduKkPafHGt9vhsrWxiM"
