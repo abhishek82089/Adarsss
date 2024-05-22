e213d3e2d6e66750bbac8084d7e9b751c015f539
Abhishek <126752231+abhishek82089@users.noreply.github.com>
2024-05-22 20:38:15 +0530

    Update vars.py

--- modules/vars.py
+++ modules/vars.py
@@ -1,3 +1,3 @@
-api_id ="23421759"# "25298674"
-api_hash ="ba8ae5b1f703fc771d1e8425db8752d1" # "1a7e01278b2bf0fe975fb8ce7c5ead8d"
-bot_token ="7129059177:AAFVZ8VQZn6VaeinkkXAgHneWySAbMrD36M"# "7092396095:AAF_jP7qZx_cC0esduKkPafHGt9vhsrWxiM"
+api_id ="21311076"
+api_hash ="7e542efdc73d08a4b382b5d6754c072d"
+bot_token ="6813417286:AAH134ybixPBDPco_zhIRyuu6VXj71Aw8tE"
